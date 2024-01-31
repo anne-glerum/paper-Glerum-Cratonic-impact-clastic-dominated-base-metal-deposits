6aff92a865ea43fbcfb692d23db41f88267efb97
anne-glerum <a.c.glerum@uu.nl>
2024-01-31 12:45:40 +0100

    Make init topo output name dependent on craton specs

--- initial_topography.py
+++ initial_topography.py
@@ -12,10 +12,7 @@ rc("font", size=20)
 rc("axes", titlesize=25)
 rc("legend", fontsize=10)
 
-output_file_name = "5p_fixed_perturbation_craton_UC23km.txt"
-output_file_name = "5p_fixed_craton_UC23km_400km.txt"
-# Remove existing version
-#os.remove(output_file_name)
+output_file_name = "5p_fixed_craton_UC23km"
 
 dx = 312.5
 x_width = 700e3
@@ -25,8 +22,8 @@ topo_craton = 360 #450 #750 #m
 topo_pert = 0 #1040 #m
 topo_normal = 0 #m
 rift_axis = 350e3 #m
-craton_edge = 400e3 #450e3 #m
-sigma_polygon = 10e3 #m 
+craton_edge = 500e3 #m
+sigma_polygon = 5e3 #m 
 sigma_rift = 60e3 #m
 
 topo = (1-np.exp(-((x-rift_axis)**2)/(2.0*(sigma_rift**2)))) *((0.5+0.5*np.tanh((x-craton_edge)/sigma_polygon))*topo_craton +(0.5-0.5*np.tanh((x-craton_edge)/sigma_polygon))*topo_normal)+(topo_pert * (np.exp(-((x-rift_axis)**2)/(2.0*(sigma_rift**2)))))
@@ -41,5 +38,5 @@ with open(output_file_name, 'a') as file:
     file.write('# POINTS: ' + str(int(x_width/dx)+1) + '\n')
 
 
-# Append data to existing file
-dataframe.to_csv(output_file_name,index=False, mode='a', header=False, sep=' ')
+# Write data to existing file
+dataframe.to_csv(output_file_name + "_edge" + str(craton_edge) + "_sigma" + str(sigma_polygon) + ".txt",index=False, mode='w', header=False, sep=' ')
