ff681f402168b49d08f904939b990d2b3b911485
anne-glerum <a.c.glerum@uu.nl>
2024-02-01 10:22:42 +0100

    Fix header initial topography file

--- initial_topography.py
+++ initial_topography.py
@@ -12,7 +12,7 @@ rc("font", size=20)
 rc("axes", titlesize=25)
 rc("legend", fontsize=10)
 
-output_file_name = "5p_fixed_craton_UC23km"
+base_file_name = "5p_fixed_craton_UC23km"
 
 dx = 312.5
 x_width = 700e3
@@ -22,9 +22,15 @@ topo_craton = 360 #450 #750 #m
 topo_pert = 0 #1040 #m
 topo_normal = 0 #m
 rift_axis = 350e3 #m
-craton_edge = 500e3 #m
-sigma_polygon = 5e3 #m 
+craton_edge = 400e3 #m
+sigma_polygon = 10e3 #m 
 sigma_rift = 60e3 #m
+
+# delete old file if it exists
+total_file_name = base_file_name + "_edge" + str(craton_edge) + "_sigma" + str(sigma_polygon) + ".txt"
+file_exists = os.path.exists(total_file_name)
+if file_exists:
+  os.remove(total_file_name)
 
 topo = (1-np.exp(-((x-rift_axis)**2)/(2.0*(sigma_rift**2)))) *((0.5+0.5*np.tanh((x-craton_edge)/sigma_polygon))*topo_craton +(0.5-0.5*np.tanh((x-craton_edge)/sigma_polygon))*topo_normal)+(topo_pert * (np.exp(-((x-rift_axis)**2)/(2.0*(sigma_rift**2)))))
 
@@ -34,9 +40,10 @@ dataframe = pd.DataFrame(data=data)
 
 print ("Dataframe: ", dataframe)
 
-with open(output_file_name, 'a') as file:
+with open(total_file_name, 'a') as file:
     file.write('# POINTS: ' + str(int(x_width/dx)+1) + '\n')
 
 
 # Write data to existing file
-dataframe.to_csv(output_file_name + "_edge" + str(craton_edge) + "_sigma" + str(sigma_polygon) + ".txt",index=False, mode='w', header=False, sep=' ')
+dataframe.to_csv(total_file_name,index=False, mode='a', header=False, sep=' ')
+print ("Created " + total_file_name)
