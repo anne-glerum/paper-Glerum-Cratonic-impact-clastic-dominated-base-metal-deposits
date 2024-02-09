b69f0fe9659b20b88fe994fe10da110e18b38ccc
anne-glerum <a.c.glerum@uu.nl>
2024-02-09 18:00:35 +0100

    Add comment

--- get_FS.sh
+++ get_FS.sh
@@ -10,6 +10,7 @@ do
     mkdir $name"/VTK"
   fi
 
+  # get snapshots at 1, 2, 3, 4, 5, 10, 15, 20, 25 My
   cp "/Users/acglerum/Documents/Postdoc/SB_CRYSTALS/HLRN/HLRN/FastScapeASPECT_cratons/"$name"/VTK/Topography000"{0289,0575,0861,1147,1432,2861,4289,5718,7147}".vtk" $name"/VTK/"
 
 done < list_of_model_names.txt
