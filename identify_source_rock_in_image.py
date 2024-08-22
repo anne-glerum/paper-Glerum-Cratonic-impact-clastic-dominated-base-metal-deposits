4351f9952fe816b6d7ffd205d28c3478c1ee545e
anne-glerum <a.c.glerum@uu.nl>
2024-08-22 15:12:12 +0200

    Find and draw source contours

--- identify_source_rock_in_image.py
+++ identify_source_rock_in_image.py
@@ -12,7 +12,7 @@ img = cv2.imread('5o_fixed_CERI_surfPnorm_htanriftcraton_inittopo_craton400000.0
 assert img is not None, "File could not be read, check with os.path.exists()"
 
 # Print the rows, columns and RGB/BGR channels of the image
-print("Image rows, columns and channels:", img.shape)
+print("Input image rows, columns and channels:", img.shape)
 
 # Build a mask where all dark green pixels are 255 and other colors are 0.
 # The two tuples provide the lower and upper bounds for dark green (0,83,0).
@@ -23,12 +23,38 @@ source_rock = cv2.inRange(img, (0, 80, 0), (5, 86, 5))
 host_rock_1 = cv2.inRange(img, (165, 217, 179), (171, 223, 185))  
 host_rock_2 = cv2.inRange(img, (108, 187, 160), (114, 193, 166))  
 host_rock_3 = cv2.inRange(img, (62, 132, 31), (68, 138, 37))  
+# Combine the three different host rocks into one.
 tmp_host_rock = cv2.bitwise_or(host_rock_1,host_rock_2)
 host_rock = cv2.bitwise_or(tmp_host_rock,host_rock_3)
 
+# Build a mask where all black pixels are 255 and other colors are 0.
+# The two tuples provide the lower and upper bounds for black (0,0,0).
+active_fault_zone = cv2.inRange(img, (0, 0, 0), (80, 80, 80))  
+
+# Build a mask where all blue pixels are 255 and other colors are 0.
+# The two tuples provide the lower and upper bounds for blue (14,50,98).
+inactive_fault_zone = cv2.inRange(img, (95, 47, 11), (101, 53, 17))  
+
+# Get the contours of source area
+#source_rock_contours, source_rock_hierarchy = cv2.findContours(source_rock, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+source_rock_contours, source_rock_hierarchy = cv2.findContours(source_rock, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+print("Number of Contours found = " + str(len(source_rock_contours)))
 
 # Subtract 1, because the background is counted.
 n_source_rock = cv2.connectedComponents(source_rock)[0] - 1
+
+# Print the number of green blobs identified
+print(f'n_labels = {n_source_rock}')
+
+# Draw the source contours.
+# This function modifies the input image, so we make a copy.
+# We also have to change this single channel copy to a 3 channel image.
+print("Source rock image rows, columns and channels:", source_rock.shape)
+source_rock_contours_image = cv2.cvtColor(source_rock.copy(), cv2.COLOR_GRAY2RGB)
+print("Source rock contour image rows, columns and channels:", source_rock_contours_image.shape)
+cv2.drawContours(source_rock_contours_image, source_rock_contours, -1, (0,255,0), 3)
+plt.imshow(source_rock_contours_image)
+
 
 # Retrieve the locations of the dark green pixels
 locations = cv2.findNonZero(source_rock)
@@ -36,15 +62,15 @@ locations = cv2.findNonZero(source_rock)
 # Plot the thresholded image
 #plt.imshow(source_rock,'gray',vmin=0,vmax=255)
 #plt.imshow(host_rock,'gray',vmin=0,vmax=255)
-plt.subplot(2,3,1),plt.imshow(source_rock,'gray',vmin=0,vmax=255)
-plt.subplot(2,3,2),plt.imshow(host_rock,'gray',vmin=0,vmax=255)
-plt.subplot(2,3,4),plt.imshow(host_rock_1,'gray',vmin=0,vmax=255)
-plt.subplot(2,3,5),plt.imshow(host_rock_2,'gray',vmin=0,vmax=255)
-plt.subplot(2,3,6),plt.imshow(host_rock_3,'gray',vmin=0,vmax=255)
+#plt.subplot(2,3,1),plt.imshow(source_rock,'gray',vmin=0,vmax=255)
+#plt.subplot(2,3,2),plt.imshow(source_rock_contour_image,vmin=0,vmax=255)
+#plt.subplot(2,3,2),plt.imshow(host_rock,'gray',vmin=0,vmax=255)
+#plt.subplot(2,3,3),plt.imshow(inactive_fault_zone,'gray',vmin=0,vmax=255)
+#plt.subplot(2,3,4),plt.imshow(host_rock_1,'gray',vmin=0,vmax=255)
+#plt.subplot(2,3,5),plt.imshow(host_rock_2,'gray',vmin=0,vmax=255)
+#plt.subplot(2,3,6),plt.imshow(host_rock_3,'gray',vmin=0,vmax=255)
 
 plt.show()
 
 
-# Print the number of green blobs identified
-print(f'n_labels = {n_source_rock}')
 
